6af2c27cfd531493776b1897487ca9c6a6a53873
adam klein <adamshuv22@gmail.com>
2011-09-26 19:14:16 +0300

    initial version

---
+++ israel_invoice/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsraelInvoice::Application.config.secret_token = '925c2ad8b21356d3eec37b6ba85885de40bb98da5c6d3e708c209ba5882fd690150cd764092862af8af7463f7a90302d0ee75812547b220c8d3363b9087900c4'
